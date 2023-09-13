e1991082a0a7d401fee9ec41d0aee6d1d8362f83
Serhii Dykyi <serge@gisfiber.com>
2023-09-13 16:45:56 +0300

    set uart br 750000

--- Dyness-BMS-CAN/Dyness-BMS-CAN.ino
+++ Dyness-BMS-CAN/Dyness-BMS-CAN.ino
@@ -1,5 +1,7 @@
 #include "STM32_CAN.h"
 
+#define BAUDRATE 750000
+
 STM32_CAN Can( CAN1, ALT ); //Use PB8/9 pins for CAN1.
 static CAN_message_t CAN_RX_msg;
 
@@ -24,13 +26,17 @@ void printCanMsg() {
 HardwareSerial Serial2( PA3, PA2 );
 HardwareSerial Serial3( PB11, PB10 );
 
-uint16_t counter = 0;
+uint32_t counter = 0;
+uint64_t lastMillis = 0;
 
 void setup() {
   pinMode( LED_BUILTIN, OUTPUT );
-  Serial.begin( 921600 );
-  Serial2.begin( 921600 );
-  Serial3.begin( 921600 );
+  Serial.begin( BAUDRATE );
+  Serial2.begin( BAUDRATE );
+  Serial3.begin( BAUDRATE );
+  Serial.println( "Serial 1" );
+  Serial2.println( "Serial 2" );
+  Serial3.println( "Serial 3" );
   Can.begin();
   Can.enableSilentMode();
   Can.setBaudRate( 500000 ); //500KBPS
@@ -40,9 +46,17 @@ void loop() {
   if ( Can.read( CAN_RX_msg ) ) {
     counter++;
     digitalWrite( LED_BUILTIN, counter % 2 );
-    Serial2.println( counter );
-    Serial3.println( counter );
 
     printCanMsg();
+  }
+
+  uint64_t currMillis = millis();
+  if ( currMillis - lastMillis > 1000 ) {
+    lastMillis = currMillis;
+    counter++;
+    Serial.println( counter );
+    Serial2.println( counter );
+    Serial3.println( counter );
+    digitalWrite( LED_BUILTIN, counter % 2 );
   }
 }
